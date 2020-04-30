a7eb86e982200a9a64597425c49c15784bf797d1
arshdeep saini <saini.arshdeepsingh@gmail.com>
2020-04-29 21:07:24 -0700

    made some changes

--- Projects/HelloWorld/Program.cs
+++ Projects/HelloWorld/Program.cs
@@ -6,24 +6,8 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            string abc = "what is new";
-            string def = "everything";
-            Console.WriteLine(String.Format($"Hey how are you {abc} {def}"));
-            Console.ReadLine();
-            string how = "what is this";
-            for (int i = 0; i < how.Length; i++)
-            {
-                if (true)
-                {
-                    Console.WriteLine(" asdasdasdasdasdasdasd");
+            System.Console.WriteLine("what should i do with this");
 
-
-                    return;
-                }
-                else {
-
-                }
-            }
         }
     }
 }
