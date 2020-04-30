17cc08efdd323a0f8af73114567c39e60219d982
arshdeep saini <saini.arshdeepsingh@gmail.com>
2020-04-29 20:16:08 -0700

    initial commit from VS code

---
+++ Projects/HelloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string abc = "what is new";
+            string def = "everything";
+            Console.WriteLine(String.Format($"Hey how are you {abc} {def}"));
+            Console.ReadLine();
+            string how = "what is this";
+            for (int i = 0; i < how.Length; i++)
+            {
+                if (true)
+                {
+                    Console.WriteLine(" asdasdasdasdasdasdasd");
+
+
+                    return;
+                }
+                else {
+
+                }
+            }
+        }
+    }
+}
