80a19b766a5859d7737c8d1741750f46f7ba2e36
arshdeep saini <saini.arshdeepsingh@gmail.com>
2021-01-30 22:05:17 -0800

    new commit

--- Projects/HelloWorld/Program.cs
+++ Projects/HelloWorld/Program.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 
-namespace HelloWorld
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            System.Console.WriteLine("what should i do with this");
-
+namespace HelloWorld {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("what should i do with this");
+            Console.WriteLine ();
+            Console.
         }
     }
 }
